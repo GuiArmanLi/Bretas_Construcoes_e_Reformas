2af92d261c5319770f9d7157680799270a1e2ba5
Guilherme Santana Pessa <guilhermesantanapessa12@gmail.com>
2023-10-26 22:07:58 -0300

    -

--- Fabio/ReadMe.md
+++ Fabio/ReadMe.md
@@ -1,5 +1,5 @@
 # Site - Bretas Construções e Reformas
-
+- Link para o Site: https://bretas-construcoes.vercel.app/
 ### Tecnologias
     -HTML
     -CSS
--- produtosApi/src/main/java/com/HenriPessa/produtosApi/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.HenriPessa.produtosApi.repository;
-
-import com.HenriPessa.produtosApi.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
